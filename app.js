87fc54e059986e79374267bebc30d1bb304aac70
root <root@LAPTOP-T9HP8IPL>
2023-04-19 15:52:46 -0500

    Stable Build v2

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
 let searchButton = document.querySelector(".search-button");
-let searchBar = document.querySelector(".search-bar");
+let searchInput = document.querySelector(".user-search");
 let container = $(".character-container");
 let characterImage = document.querySelector(".image");
 
-searchButton.addEventListener("click", () => {
-    $.get('https://bobsburgers-api.herokuapp.com/characters/', (data) => {
-        
-         
-        for (let i = 0; i < 3; i++) {
-            // debugger
-            let div = $(`<div class = 'cards'></div>`)
-            
-            const name = data[i].name;
-            const image = data[i].image;
-            
-            const html = `
-            <h1 class = 'card-title'>${name}</h1>
-            <img class="image" src=${image} alt="">`
-        console.log(html)
-        div.append(html)
-        console.log(div)
-        $('.character-container').append(div);
-        }
-        
-    });
+let characters = []
 
+$.get('https://bobsburgers-api.herokuapp.com/characters/', (data) => {
+    for (let i = 0; i < data.length; i++) {
+       let div = $(`<div class = 'cards'></div>`)
+       const name = data[i].name;
+       const image = data[i].image;
+       const html = `<h1 class = 'card-title'>${name}</h1><img class="image" src=${image} alt="">`
+       div.append(html)
+       $('.character-container').append(div);
+       characters.push({name: name, image: image, element: div})
+   }
+});
+
+searchButton.addEventListener("click", () => {
+    let userSearch = searchInput.value.toLowerCase()
+    console.log(userSearch)
+    characters.forEach(character => {
+        let showCard = character.name.toLowerCase().includes(userSearch)
+        if (showCard) {
+            character.element.show()
+        } else {
+            character.element.hide()
+        }
+    })
+    
+    console.log(userSearch)
 })
